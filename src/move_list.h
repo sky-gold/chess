5fe6e4afaab653798231c05c33c9be70703e54ad
Nikita Efimov <nikita-efimov04@mail.ru>
2023-03-12 12:49:37 +0300

    move list

---
+++ src/move_list.h
@@ -0,0 +1,57 @@
+#ifndef CHESS_MOVE_LIST_H
+#define CHESS_MOVE_LIST_H
+
+#include <cstdint>
+#include <array>
+
+#include "types.h"
+
+const size_t MOVE_LIST_MAX = 218;
+
+class MoveList {
+public:
+    MoveList() = default;
+
+    void push_back(Move move) {
+        data_[len_++] = move;
+    }
+
+    Move &operator[](uint8_t index) {
+        return data_[index];
+    }
+
+    Move operator[](uint8_t index) const {
+        return data_[index];
+    }
+
+    auto begin() {
+        return data_.begin();
+    }
+
+    auto begin() const {
+        return data_.begin();
+    }
+
+    auto end() {
+        return data_.end();
+    }
+
+    auto end() const {
+        return data_.end();
+    }
+
+    size_t size() const {
+        return len_;
+    }
+
+    bool empty() const {
+        return (len_ == 0);
+    }
+
+private:
+    std::array<Move, MOVE_LIST_MAX> data_;
+    size_t len_ = 0;
+};
+
+
+#endif //CHESS_MOVE_LIST_H
