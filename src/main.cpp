dc39593e3984945f8e9b6904684cc55600d8574f
Nikita Efimov <nikita-efimov04@mail.ru>
2023-03-27 02:30:43 +0300

    main init, test and start app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <thread>
+
+#include "init.h"
+
+#include "app.h"
+
+#include "move_gen_test.h"
+
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    init::init();
+    std::cout << "Init takes " << clock() << "ms" << std::endl;
+    std::cout << "Number of threads supported by the system: " << std::thread::hardware_concurrency() << std::endl;
+    move_gen_test::test();
+//    move_gen_test::speed_test();
+    App app;
+    app.run();
     return 0;
 }
